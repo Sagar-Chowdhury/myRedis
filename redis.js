a90d20ba0d640bbcd146cbaef285e6ec0b943f11
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2024-10-19 14:04:36 +0530

    Initial Commit of Eviction Policy Change

--- redis.js
+++ redis.js
@@ -4,34 +4,54 @@ const SNAPSHOT_FILE = path.join('D:', 'myOwnRedis', 'myRedis', 'backupData', 'ba
 
 
 class Redis {
-
-  constructor() {
+  constructor(memoryLimit = 50 * 1024 * 1024, evictionPolicy = "LRU") {
     this.data = {};
     this.ttl = {};
     this.channels = {};
+    this.lastAccessTime = {};
+    this.currentMemoryUseage = 0;
+    this.memoryLimit = memoryLimit;
+    this.evictionPolicy = evictionPolicy;
 
     this.loadFromSnapshot();
   }
 
   set(key, value, expiryTime = null) {
+    const keyValuePairSize = this._estimateMemoryUsage(key, value);
+
+    // if eviction is necessary keep applying until condition is met.
+    while (this.currentMemoryUseage + keyValuePairSize > this.memoryLimit) {
+      this._applyEvictionPolicy();
+    }
+
     this.data[key] = value;
+    this.currentMemoryUseage += keySize;
+
     if (expiryTime) {
       const expireAt = Date.now() + expiryTime * 1000;
       this.ttl[key] = expireAt;
       this._scheduleExpiry(key);
     }
+    this.lastAccessTime[key] = Date.now();
   }
 
   get(key) {
     if (this._isExpired(key)) {
       return null;
     }
-    return this.data[key];
+    if (this.data[key]) {
+      this.lastAccessTime[key] = Date.now();
+      return this.data[key];
+    }
+    return null;
   }
 
   del(key) {
+    const keySize = this._estimateMemoryUsage(key, this.data[key]);
     delete this.data[key];
     delete this.ttl[key];
+    delete this.lastAccessTime[key];
+    this.currentMemoryUseage -= keySize;
   }
 
   _isExpired(key) {
@@ -56,6 +76,16 @@ class Redis {
     try {
       // Check if file exists by using fs.access, which will throw an error if the file does not exist
       await fs.access(SNAPSHOT_FILE);
+      
+       fs.stat(SNAPSHOT_FILE, (err, fileStats) => {
+        if (err) {
+          console.log(err)
+        } else {
+          const result = convertBytes(fileStats.size)
+          console.log("File Size of Loaded Snapshot: ",result);
+          
+        }
+      })
 
       // If no error, the file exists, proceed to read it
       const snapshotData = await fs.readFile(SNAPSHOT_FILE, "utf-8");
@@ -74,6 +104,24 @@ class Redis {
       }
     }
   }
+
+  convertBytes(bytes) {
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+  
+    if (bytes == 0) {
+      return "n/a"
+    }
+  
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+  
+    if (i == 0) {
+      return bytes + " " + sizes[i]
+    }
+  
+    return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i]
+  }
+  
+
 
   async saveSnapShot() {
     const snapshotData = {
@@ -108,16 +156,16 @@ class Redis {
     console.log(`Client Subscribed to channel: ${channel}`);
   }
 
-  unsubscribe(ws,channel){
-    if(this.channels[channel]){
-        this.channels[channel].delete(ws);
-        if(this.channels[channel].size === 0){
-            delete this.channels[channel];
-        }
+  unsubscribe(ws, channel) {
+    if (this.channels[channel]) {
+      this.channels[channel].delete(ws);
+      if (this.channels[channel].size === 0) {
+        delete this.channels[channel];
+      }
     }
     console.log(`Client Unsunbscribed to Channel: ${channel} `);
   }
-  
+
   publish(channel, message) {
     if (this.channels[channel]) {
       this.channels[channel].forEach((client) => {
@@ -130,16 +178,52 @@ class Redis {
     }
   }
 
-  disconnect(ws){
-    for(const channel in this.channel){
-        this.channels[channel].delete(ws);
-        if(this.channels[channel].size === 0){
-            delete this.channels[channel];
-        }
+  disconnect(ws) {
+    for (const channel in this.channel) {
+      this.channels[channel].delete(ws);
+      if (this.channels[channel].size === 0) {
+        delete this.channels[channel];
+      }
     }
-  }  
+  }
   /***                                      */
 
+  /** Eviction and Memory Related Methods  ****/
+
+  _estimateMemoryUsage(key, value) {
+    const keySize = Buffer.byteLength(key, "utf8");
+    const valueSize = Buffer.byteLength(JSON.stringify(value), "utf8");
+    return keySize + valueSize;
+  }
+
+  _applyEvictionPolicy() {
+    switch (this.evictionPolicy) {
+      case "LRU":
+        this._evictLRU();
+        break;
+      default:
+        throw new Error("Invalid Eviction Policy");
+    }
+  }
+
+  _evictLRU() {
+    let lruKey = null;
+    let oldestAccessTime = Infinity;
+
+    for (const key in this.lastAccessTime) {
+      if (this.lastAccessTime[key] < oldestAccessTime) {
+        oldestAccessTime = this.lastAccessTime[key];
+        lruKey = key;
+      }
+    }
+
+    if (lruKey) {
+      this.del(lruKey);
+      console.log(`Evicted LRU key : ${lruKey}`);
+    }
+  }
+
+  /***            */
 }
 
 module.exports = Redis;
